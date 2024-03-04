be30271794b1d0348d9483e6f7a45715e5bab7cd
mmm2233 <2350617910@qq.com>
2024-03-04 09:56:06 +0800

    前端模版删减

--- mmmbi-frond/config/defaultSettings.ts
+++ mmmbi-frond/config/defaultSettings.ts
@@ -15,7 +15,7 @@ const Settings: ProLayoutProps & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'Ming 智能BI',
   pwa: true,
   logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
--- mmmbi-frond/src/pages/User/Login/index.tsx
+++ mmmbi-frond/src/pages/User/Login/index.tsx
@@ -151,8 +151,8 @@ const Login: React.FC = () => {
             maxWidth: '75vw',
           }}
           logo={<img alt="logo" src="/logo.svg" />}
-          title="Ant Design"
-          subTitle={'Ant Design 是西湖区最具影响力的 Web 设计规范'}
+          title="Ming 智能BI"
+          subTitle={'Ming 智能BI 是西湖区最具影响力的 Web 设计规范'}
           initialValues={{
             autoLogin: true,
           }}
